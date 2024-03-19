961cc2d558098bbd7ec801bf77853858a4e6c472
Erick Meyer <eagalvez129@gmail.com>
2024-03-19 15:08:07 +0100

    random choice

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ markusLoves = ["python", "Bread", "Cake", "Water", "Cats", "Dogs"]
 def lovesMore(ele):
     print("Markus loves Java more than " + ele + ".")
 
-for item in markusLoves:
-    lovesMore(item)
+for _ in range(len(markusLoves)):
+    lovesMore(random.choice(markusLoves))
+  
