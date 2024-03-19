815c81931cad3a50dd35f9612acc655dff7d17ad
BenjaminHauf <benx@ben-hauf.de>
2024-03-19 14:24:21 +0100

    A injcredible meaningfull commit message!

---
+++ app.py
@@ -0,0 +1,7 @@
+markusLoves = ["python","Bread","Cake","Water","Cats","Dogs"]
+
+def lovesMore(eleme):
+    for x in eleme
+    print("Markus loves Java more than " + ele + ".")
+
+lovesMore(markusLoves)
