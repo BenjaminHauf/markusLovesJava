76da4508edecd246e16a7240378a65e83dfb0ef7
BenjaminHauf <benx@ben-hauf.de>
2024-03-19 15:02:38 +0100

    changes

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ import random
 markusLoves = ["python", "Bread", "Cake", "Water", "Cats", "Dogs"]
 
 def lovesMore(ele):
-    print("Markus loves Java more than " + ele + ".")
+    #print("Markus loves Java more than " + ele + ".")
+    with open("README.md", "a") as file:
+        file.write("Markus loves Java more than " + ele + ".\n")
 
-for item in markusLoves:
-    lovesMore(item)
-markusLovescars = ["ford", "Volvo", "BMW"]
-
-def lovesMor(ele):
-    print("Markus loves Cars more than Python " + ele +".")
-
-    for item in markusLoves:
-        lovesMore(item)
+for item in range(len(markusLoves)):
+    lovesMore(random.choice(markusLoves))
